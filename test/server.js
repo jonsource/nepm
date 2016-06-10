943c2664f1e97e8b6c48ac7ae1b0bf2c3f58fe33
Jan Sourek <jan.sourek@gmail.com>
2016-06-10 11:24:47 +0200

    added simple test

---
+++ test/server.js
@@ -0,0 +1,15 @@
+var request = require('request')
+var expect = require('chai').expect
+
+describe("App server tests", function() {
+
+    var base_url = "http://localhost:3000/";
+    console.log(base_url)
+
+    it("returns status 200", function(done) {
+        request(base_url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+});
