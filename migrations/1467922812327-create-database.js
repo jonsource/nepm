0b8e94704df8172e4deed166645a910fb4351ae1
Jan Sourek <jan.sourek@gmail.com>
2016-07-08 01:58:10 +0200

    WIP fill dummy data

--- migrations/1467922812327-create-database.js
+++ migrations/1467922812327-create-database.js
@@ -2,121 +2,148 @@ var mysql = require('mysql');
 var dbconfig = require('../config/database');
 dbconfig.connection.multipleStatements = true;
 var connection = mysql.createConnection(dbconfig.connection);
+var async = require('async');
 
 exports.up = function(next){
-  	connection.query('CREATE DATABASE ' + dbconfig.database);
-	connection.query('USE ' + dbconfig.database +';');
-	console.log('Success: Database Created!');
-
-	connection.query('\
-	CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
-	    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-	    `username` VARCHAR(20) NOT NULL, \
-	    `password` CHAR(60) NOT NULL, \
-	        PRIMARY KEY (`id`), \
-	    UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-	    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
-	)');
-
-	connection.query('\
-	CREATE TABLE `customer` ( \
-			`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
-			`name` VARCHAR(64) NOT NULL, \
-			`email` VARCHAR(64) NOT NULL, \
-			PRIMARY KEY (`id`) \
-	)');
-
-	connection.query('\
-	CREATE TABLE `order` ( \
-			`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
-			`customer_id` INTEGER NULL DEFAULT NULL, \
-			PRIMARY KEY (`id`) \
-	)');
-
-	connection.query(' \
-	CREATE TABLE `order_item` (\
+  	
+	async.series([
+	function(callback) {
+		connection.query('CREATE DATABASE ' + dbconfig.database, callback);	
+	},
+	function(callback) {
+		connection.query('USE ' + dbconfig.database +';', function(err, result) {
+			console.log('Success: Database Created!');	
+			callback(null, 'Success: Database Created!');
+		});
+	},
+	function(callback) {
+		connection.query('\
+		CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
+		    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+		    `username` VARCHAR(20) NOT NULL, \
+		    `password` CHAR(60) NOT NULL, \
+		        PRIMARY KEY (`id`), \
+		    UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
+		    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
+		)', callback);
+	},
+	function(callback) {
+		connection.query('\
+			CREATE TABLE `customer` ( \
+				`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
+				`name` VARCHAR(64) NOT NULL, \
+				`email` VARCHAR(64) NOT NULL, \
+				PRIMARY KEY (`id`) \
+		)', callback);
+	},
+	function(callback) {	
+		connection.query('\
+			CREATE TABLE `order` ( \
+				`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
+				`customer_id` INTEGER NULL DEFAULT NULL, \
+				PRIMARY KEY (`id`) \
+			)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		CREATE TABLE `order_item` (\
 			`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
 			`order_id` INTEGER NOT NULL, \
-		`product_id` INTEGER NULL DEFAULT NULL, \
-		`order_item_option_id` INTEGER NULL DEFAULT NULL, \
-		`quantity` INTEGER NOT NULL, \
-		PRIMARY KEY (`id`) \
-	)');
-
-	connection.query(' \
-	CREATE TABLE `product` ( \
+			`product_id` INTEGER NULL DEFAULT NULL, \
+			`order_item_option_id` INTEGER NULL DEFAULT NULL, \
+			`quantity` INTEGER NOT NULL, \
+			PRIMARY KEY (`id`) \
+		)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		CREATE TABLE `product` ( \
 			`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
 			`parent_id` INTEGER NULL DEFAULT NULL, \
 			`price` DECIMAL(6.2) NULL DEFAULT NULL, \
 			`name` VARCHAR(64) NULL DEFAULT NULL, \
 			`descirption` MEDIUMTEXT NULL DEFAULT NULL, \
 			PRIMARY KEY (`id`) \
-	)');
-
-	connection.query(' \
-	CREATE TABLE `variant` ( \
+		)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		CREATE TABLE `variant` ( \
 			`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
 			`name` VARCHAR(32) NOT NULL, \
 			PRIMARY KEY (`id`) \
-	)');
-
-	connection.query(' \
-	CREATE TABLE `option` ( \
-	  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
+		)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		CREATE TABLE `option` ( \
+	  		`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
 			`variant_id` INTEGER NULL DEFAULT NULL, \
 			`name` VARCHAR(32) NOT NULL, \
 			PRIMARY KEY (`id`) \
-	)');
-
-	connection.query(' \
-	CREATE TABLE `product_has_variant` ( \
+		)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		CREATE TABLE `product_has_variant` ( \
 			`product_id` INTEGER NULL DEFAULT NULL, \
 			`variant_id` INTEGER NULL DEFAULT NULL, \
 			PRIMARY KEY (`product_id`, `variant_id`) \
-	)');
-
-	connection.query(' \
-	CREATE TABLE `product_has_option` ( \
+		)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		CREATE TABLE `product_has_option` ( \
 			`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
 			`product_id` INTEGER NULL DEFAULT NULL, \
 			`option_id` INTEGER NULL DEFAULT NULL, \
 			`price` DECIMAL(6.2) NULL DEFAULT NULL, \
 			PRIMARY KEY (`id`) \
-	)');
-
-	connection.query(' \
-	CREATE TABLE `order_item_option` ( \
+		)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		CREATE TABLE `order_item_option` ( \
 			`id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, \
 			`variant_name` VARCHAR(32) NOT NULL, \
 			`option_name` VARCHAR(32) NOT NULL, \
 			`price` DECIMAL(6.2) NULL DEFAULT NULL, \
 			PRIMARY KEY (`id`) \
-	)');
-
-	connection.query(' \
-	ALTER TABLE `order` ADD FOREIGN KEY (customer_id) REFERENCES `customer` (`id`);\
-	');
-	connection.query(' \
-	ALTER TABLE `order_item` ADD FOREIGN KEY (order_id) REFERENCES `order` (`id`);\
-	');
-	connection.query(' \
-	ALTER TABLE `order_item` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);\
-	ALTER TABLE `order_item` ADD FOREIGN KEY (order_item_option_id) REFERENCES `order_item_option` (`id`);\
-	ALTER TABLE `product` ADD FOREIGN KEY (parent_id) REFERENCES `product` (`id`);\
-	ALTER TABLE `option` ADD FOREIGN KEY (variant_id) REFERENCES `variant` (`id`);\
-	ALTER TABLE `product_has_variant` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);\
-	ALTER TABLE `product_has_variant` ADD FOREIGN KEY (variant_id) REFERENCES `variant` (`id`);\
-	ALTER TABLE `product_has_option` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);\
-	ALTER TABLE `product_has_option` ADD FOREIGN KEY (option_id) REFERENCES `option` (`id`);\
-	');
-
-	console.log('Success: Tables and keys updated!')
-  	next();
+		)', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		ALTER TABLE `order` ADD FOREIGN KEY (customer_id) REFERENCES `customer` (`id`);', callback);
+	},
+	function(callback) {
+		connection.query(' \
+		ALTER TABLE `order_item` ADD FOREIGN KEY (order_id) REFERENCES `order` (`id`);', callback);
+	},
+	function(callback) {
+		connection.query(' \
+			ALTER TABLE `order_item` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);\
+			ALTER TABLE `order_item` ADD FOREIGN KEY (order_item_option_id) REFERENCES `order_item_option` (`id`);\
+			ALTER TABLE `product` ADD FOREIGN KEY (parent_id) REFERENCES `product` (`id`);\
+			ALTER TABLE `option` ADD FOREIGN KEY (variant_id) REFERENCES `variant` (`id`);\
+			ALTER TABLE `product_has_variant` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);\
+			ALTER TABLE `product_has_variant` ADD FOREIGN KEY (variant_id) REFERENCES `variant` (`id`);\
+			ALTER TABLE `product_has_option` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);\
+			ALTER TABLE `product_has_option` ADD FOREIGN KEY (option_id) REFERENCES `option` (`id`);',
+			function(err, results) {
+				callback(null, "foreign keys added");
+		});
+	}],
+	function(err, results) {
+		console.log(results);
+		console.log('Success: Tables and keys updated!')
+  		next();
+	});
 };
 
 exports.down = function(next){
-  	connection.query('DROP DATABASE ' + dbconfig.database);
-		
-	console.log('Success: Database Dropped!');
-  	next();
+  	connection.query('DROP DATABASE ' + dbconfig.database, function(err, result) {
+  		console.log(result);
+  		console.log('Success: Database Dropped!');
+  		next();
+  	});
 };
---
+++ scripts/fill_data.js
@@ -0,0 +1,109 @@
+var mysql = require('mysql');
+var dbconfig = require('../config/database');
+dbconfig.connection.multipleStatements = true;
+var connection = mysql.createConnection(dbconfig.connection);
+var async = require('async');
+
+connection.query('USE ' + dbconfig.database +';');
+
+function create_products(callback) {
+	connection.query(' \
+	INSERT INTO `product` (`id`,`price`, `name`, `descirption`) VALUES\
+		(1, 10.0, "t-shirt", "Obyčejné triko"),\
+		(2, 25.0, "trousers", "Obyčejné kalhoty"),\
+		(3, 15.0, "shirt", "Obyčejná košile");\
+	', callback);
+}
+
+function create_variants(callback) {
+	connection.query(' \
+	INSERT INTO `variant` (`id`, `name`) VALUES\
+		(1, "velikost"),\
+		(2, "barva");\
+	', callback);
+}
+
+function create_options(callback) {
+	connection.query(' \
+	INSERT INTO `option` (`id`, `variant_id`, `name`) VALUES\
+		(1, 1, "s"),\
+		(2, 1, "m"),\
+		(3, 1, "l"),\
+		(4, 1, "xl"),\
+		(5, 2, "červená"),\
+		(6, 2, "modrá"),\
+		(7, 2, "bílá");\
+	', callback);
+}
+
+function create_product_variants(callback) {
+	connection.query(' \
+	INSERT INTO `product_has_variant` (`product_id`, `variant_id`) VALUES\
+		(1, 1),\
+		(1, 2),\
+		(2, 1),\
+		(2, 2),\
+		(3, 1);\
+	', callback);
+}
+
+function create_product_options(callback) {
+	connection.query(' \
+	INSERT INTO `product_has_option` (`id`, `product_id`, `option_id`, `price`) VALUES\
+		(1, 1, 1, 0.0),\
+		(2, 1, 2, 0.0),\
+		(3, 1, 3, 0.5),\
+		(4, 1, 4, 1.0),\
+		(5, 1, 5, 0.0),\
+		(6, 1, 6, 0.0),\
+		(7, 1, 7, 0.0),\
+		(8, 2, 1, 0.0),\
+		(9, 2, 2, 0.0),\
+		(10, 2, 3, 1.0),\
+		(11, 2, 4, 2.0),\
+		(12, 2, 5, 0.0),\
+		(13, 2, 6, 0.0),\
+		(14, 2, 7, 2.0),\
+		(15, 3, 1, 0.0),\
+		(16, 3, 2, 0.5),\
+		(17, 3, 3, 1.0),\
+		(18, 3, 4, 2.5);\
+	', callback);
+}
+
+async.series([
+	function(callback) {
+		connection.query('SHOW TABLES', function(err, results) {
+			console.log(results);
+			callback(null);
+		});
+	},
+	function(callback) {
+		connection.query('\
+			SET foreign_key_checks = 0;\
+			TRUNCATE TABLE product_has_option;\
+			TRUNCATE TABLE product_has_variant;\
+			TRUNCATE TABLE `option`;\
+			TRUNCATE TABLE `variant`;\
+			TRUNCATE TABLE `product`;\
+			SET foreign_key_checks = 1;\
+			', function(err, results) {
+			console.log('Deleted', results);
+			callback(null);
+		});
+	},
+	create_products, create_variants, create_options, create_product_variants, create_product_options],
+	function(err, results) {
+	
+		if (err) throw err;
+
+		connection.query('SELECT * FROM product AS p\
+			LEFT JOIN product_has_variant AS pv ON pv.product_id = p.id\
+			LEFT JOIN variant AS v ON v.id=pv.variant_id', function(err, results) {
+			if (err) throw err;
+
+			console.log('done');
+			console.log(results);
+
+	});
+})
